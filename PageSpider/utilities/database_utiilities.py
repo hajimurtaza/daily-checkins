7de9352dd19e3cef9e1c179317f7f69170e13ad8
Murtaza <murtazahaji2@gmail.com>
2018-02-08 18:27:29 -0500

    initial commit

---
+++ PageSpider/utilities/database_utiilities.py
@@ -0,0 +1,8 @@
+def create_database(database_path : str):
+    #TODO: Generate the database here
+    pass
+
+def save_words_database(database_path: str, words_list: list):
+    #TODO: Save words to database here
+    pass
+
