af23ac47a190797ab8a8928e3bc94fc19f895ed5
Murtaza <murtazahaji2@gmail.com>
2018-02-08 19:14:31 -0500

    libraries

--- PageSpider/utilities/database_utiilities.py
+++ PageSpider/utilities/database_utiilities.py
@@ -1,8 +1,8 @@
-def create_database(database_path : str):
-    #TODO: Generate the database here
+def create_database(database_path: str):
+    # TODO: Generate the database here
     pass
+
 
 def save_words_database(database_path: str, words_list: list):
-    #TODO: Save words to database here
+    # TODO: Save words to database here
     pass
-
--- PageSpider/utilities/url_utilities.py
+++ PageSpider/utilities/url_utilities.py
@@ -1,12 +1,13 @@
 def load_url_from_file(file_path: str):
-    #TODO : Add code needed to read urls from file
+    # TODO : Add code needed to read urls from file
     pass
+
 
 def load_pages(url: str):
-    #TODO: Add code that reads the url
+    # TODO: Add code that reads the url
     pass
+
 
 def scrape_page(page_contents: str):
-    #TODO: Analyse the text
+    # TODO: Analyse the text
     pass
-
